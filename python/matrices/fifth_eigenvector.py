51c8e3ca7b15c18de876f8e7348fa96af8a1c0c0
Matthew <vonmas360@gmail.com>
2026-04-28 06:50:53 -0500

    Matrices

--- python/matrices/fifth_eigenvector.py
+++ python/matrices/fifth_eigenvector.py
@@ -1,1 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+n = 5
+h_step = 1 / (n + 1)
+scaling_factor = 1 / (2 * (h_step**2))
+
+main_diag = np.diagflat(2 * np.ones(n))
+off_diag_low = np.diagflat(-1 * np.ones(n - 1), k=-1)
+off_diag_high = np.diagflat(-1 * np.ones(n - 1), k=1)
+
+H = scaling_factor * (main_diag + off_diag_low + off_diag_high)
+
+eigenvalues, eigenvectors = np.linalg.eig(H)
+
+idx = eigenvalues.argsort()
+eigenvectors = eigenvectors[:, idx]
+
+fifth_eigenvector = eigenvectors[:, 4]
+
+x_values = np.linspace(1/6, 5/6, 5)
+x_continuous = np.linspace(0, 1, 100)
+y_continuous = np.sqrt(2) * np.sin(np.pi * x_continuous)
+
+plt.figure(figsize=(8, 5))
+plt.plot(x_continuous, y_continuous, label=r'$\sqrt{2}\sin(\pi x)$', color='blue')
+plt.scatter(x_values, fifth_eigenvector, color='red', label='5th Eigenvector')
+plt.grid(True)
+plt.show()
--- python/matrices/tenth_eigenvector.py
+++ python/matrices/tenth_eigenvector.py
@@ -1,1 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+matrix_dimension = 10
+
+n = matrix_dimension
+h = 1 / (n + 1)
+scaling = 1 / (2 * (h**2))
+
+matrix = scaling * (np.diagflat(2 * np.ones(n)) +
+                    np.diagflat(-1 * np.ones(n - 1), 1) +
+                    np.diagflat(-1 * np.ones(n - 1), -1))
+
+val, vec = np.linalg.eig(matrix)
+
+tenth_vec = vec[:, n - 1]
+
+x_vals = np.linspace(1/(n+1), n/(n+1), n)
+x_cont = np.linspace(0, 1, 100)
+y_cont = np.sqrt(2) * np.sin(np.pi * x_cont)
+
+plt.plot(x_cont, y_cont)
+plt.scatter(x_vals, tenth_vec)
+plt.show()
